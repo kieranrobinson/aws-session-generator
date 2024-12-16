9b41eef145de2bff64b10cd15ee8006938b975b2
Kieran Robinson <robinson.kieran@outlook.com>
2024-12-16 22:26:57 +0000

    Run initial config PowerShell commands and ask for MFA code

--- AWSSessionGenerator/Program.cs
+++ AWSSessionGenerator/Program.cs
@@ -8,10 +8,15 @@ class Program
         string awsSecretKey = "secretkey";
         string mfaSerial = "mfaserial";
         string region = "eu-west-1";
+        string outputType = "json";
     
+        RunPowershellCommand($"aws configure set aws_access_key_id {awsAccessKey}");
+        RunPowershellCommand($"aws configure set aws_secret_key {awsSecretKey}");
+        RunPowershellCommand($"aws configure set region {region}");
+        RunPowershellCommand($"aws configure set output {outputType}");
+        
         Console.Write("Enter MFA Code: ");
         string mfaCode = Console.ReadLine();
-        
     }
 
     private static string RunPowershellCommand(string command)
