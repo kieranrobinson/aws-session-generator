67d94a1e2212f0a58150302c490fff8464560095
Kieran Robinson <robinson.kieran@outlook.com>
2024-12-16 22:19:48 +0000

    Add ability to run PowerShell command

--- AWSSessionGenerator/Program.cs
+++ AWSSessionGenerator/Program.cs
@@ -1,7 +1,43 @@
-﻿static void Main(string[] args)
+﻿using System.Management.Automation;
+
+class Program
 {
-    string awsAccessKey = "accesskey";
-    string awsSecretKey = "secretkey";
-    string mfaSerial = "mfaserial";
-    string region = "eu-west-1";
+    static void Main(string[] args)
+    {
+        string awsAccessKey = "accesskey";
+        string awsSecretKey = "secretkey";
+        string mfaSerial = "mfaserial";
+        string region = "eu-west-1";
+    
+        Console.Write("Enter MFA Code: ");
+        string mfaCode = Console.ReadLine();
+        
+    }
+
+    private static string RunPowershellCommand(string command)
+    {
+        using (PowerShell powerShell = PowerShell.Create())
+        {
+            powerShell.AddScript(command);
+            var results = powerShell.Invoke();
+
+            if (powerShell.HadErrors)
+            {
+                foreach (var error in powerShell.Streams.Error)
+                {
+                    Console.WriteLine(error.ToString());
+                }
+                throw new Exception("PowerShell Error");
+            }
+
+            string output = "";
+            foreach (var result in results)
+            {
+                output += result.ToString() + Environment.NewLine;
+            }
+
+            return output;
+        }
+    }
 }
+
