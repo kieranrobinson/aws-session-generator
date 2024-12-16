c58d8670a62eb61c96f218b549dc974f485213ce
Kieran Robinson <robinson.kieran@outlook.com>
2024-12-16 22:41:29 +0000

    Add exception handling and more progress on workflow

--- AWSSessionGenerator/Program.cs
+++ AWSSessionGenerator/Program.cs
@@ -9,14 +9,37 @@ class Program
         string mfaSerial = "mfaserial";
         string region = "eu-west-1";
         string outputType = "json";
-    
-        RunPowershellCommand($"aws configure set aws_access_key_id {awsAccessKey}");
-        RunPowershellCommand($"aws configure set aws_secret_key {awsSecretKey}");
-        RunPowershellCommand($"aws configure set region {region}");
-        RunPowershellCommand($"aws configure set output {outputType}");
         
-        Console.Write("Enter MFA Code: ");
-        string mfaCode = Console.ReadLine();
+        try
+        {
+            RunPowershellCommand($"aws configure set aws_access_key_id {awsAccessKey}");
+            RunPowershellCommand($"aws configure set aws_secret_key {awsSecretKey}");
+            RunPowershellCommand($"aws configure set region {region}");
+            RunPowershellCommand($"aws configure set output {outputType}");
+            
+            Console.Write("Enter MFA Code: ");
+            string? mfaCode = Console.ReadLine();
+
+            while (string.IsNullOrWhiteSpace(mfaCode))
+            {
+                Console.WriteLine("MFA Code is required.");
+                Console.Write("Enter MFA Code: ");
+                mfaCode = Console.ReadLine();
+            }
+            
+            Console.WriteLine("Retrieving Session Token...");
+            string sessionTokenResult =
+                RunPowershellCommand($"aws sts get-session-token --serial-number {mfaSerial} --token-code {mfaCode}");
+            Console.WriteLine("Token retrieved.");
+            Console.WriteLine("Setting Temporary Session Credentials...");
+            
+            //TODO: Set temporary session credentials
+            
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An Error has occured: {ex.Message}");
+        }
     }
 
     private static string RunPowershellCommand(string command)
