5d276eef4055722a9720f52cafaac62531c6c821
Kieran Robinson <robinson.kieran@outlook.com>
2024-12-16 22:08:22 +0000

    Initial Commit

---
+++ AWSSessionGenerator/Program.cs
@@ -0,0 +1,7 @@
+﻿static void Main(string[] args)
+{
+    string awsAccessKey = "accesskey";
+    string awsSecretKey = "secretkey";
+    string mfaSerial = "mfaserial";
+    string region = "eu-west-1";
+}
